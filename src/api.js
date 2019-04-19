5bc2aae4b838fd5c2e743e69c5e828e907a48ee1
jcwrightson <jcwrightson@gmail.com>
2019-04-19 18:23:03 +0200

    linting

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import fetch from "cross-fetch"
 
 export const url = "http://localhost:3000/graphql"
 
+/* eslint-disable-next-line no-shadow */
 export const doFetch = async (url, query, variables) => {
 	const results = await fetch(url, {
 		method: "POST",
